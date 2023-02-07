ee59ef8c96de988dd16d719f6ff15571a6f22fea
campisir <rc113@students.uwf.edu>
2023-02-06 23:43:38 -0500

    api connection + username storage

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http'
+import { lastValueFrom } from 'rxjs';
+
+interface User{
+  name: string
+  email: string
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +13,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'client';
+  public loginname = ''
+  public submitname = ''
+  public submittedname = ''
+  public success = false
+  public addsuccess = false
+  title = 'SoundSpace';
+  public users: User[] = []
+  
+  constructor(
+    private httpClient: HttpClient
+  ){}
+
+  async checkUser()
+  {
+    this.success = false;
+   const users$ = await this.httpClient.get<User[]>('/api/users', {
+   })
+   this.users = await lastValueFrom(users$)
+
+   for (var user of this.users) {
+    if(user.name == this.loginname)
+    {
+      this.success = true;
+    }
+  }
+   this.loginname = ''
+  }
+
+  async addUser()
+  {
+    this.submittedname = this.submitname
+    this.addsuccess = true
+    this.httpClient.post("/api/users", {
+      name: this.submitname
+    }).subscribe()
+
+   this.submitname = ''
+  }
+
 }
+
