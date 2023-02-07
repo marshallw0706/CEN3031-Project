f753675b678f59bc76c2aa65d99835be29d2f291
campisir <rc113@students.uwf.edu>
2023-02-06 23:12:02 -0500

    project skeleton

---
+++ api/main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func initializeRouter() {
+	router := mux.NewRouter()
+	router.HandleFunc("/api/users", GetUsers).Methods("GET")
+	router.HandleFunc("/api/users/{id}", GetUser).Methods("GET")
+	router.HandleFunc("/api/users", CreateUser).Methods("POST")
+	router.HandleFunc("/api/users/{id}", UpdateUser).Methods("PUT")
+	router.HandleFunc("/api/users/{id}", DeleteUser).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":5000", router))
+}
+func main() {
+	InitialMigration()
+	initializeRouter()
+}
+
+var DB *gorm.DB
+var err error
+
+const DSN = "root:password@tcp(localhost:3306)/sys?charset=utf8&parseTime=true"
+
+type User struct {
+	gorm.Model
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
+
+func InitialMigration() {
+	DB, err = gorm.Open(mysql.Open(DSN), &gorm.Config{})
+	if err != nil {
+		panic("Cannot connect to DB")
+	}
+	DB.AutoMigrate(&User{})
+}
+func GetUsers(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var users []User
+	DB.Find(&users)
+	json.NewEncoder(w).Encode(users)
+}
+
+func GetUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	var user User
+	DB.First(&user, params["id"])
+	json.NewEncoder(w).Encode(user)
+}
+
+func CreateUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var user User
+	json.NewDecoder(r.Body).Decode(&user)
+	DB.Create(&user)
+	json.NewEncoder(w).Encode(user)
+}
+
+func UpdateUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	var user User
+	DB.First(&user, params["id"])
+	json.NewDecoder(r.Body).Decode(&user)
+	DB.Save(&user)
+	json.NewEncoder(w).Encode(user)
+
+}
+
+func DeleteUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	var user User
+	DB.Delete(&user, params["id"])
+	json.NewEncoder(w).Encode("The User has been deleted")
+}
