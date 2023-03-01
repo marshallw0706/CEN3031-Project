fc9b69ef35e391f43675abdf56af5aab6c3a07fb
sofiacelestrin <101586639+sofiacelestrin@users.noreply.github.com>
2023-03-01 18:21:51 -0500

    Update home.component.spec.ts

--- client/src/app/side-bar/home/home.component.spec.ts
+++ client/src/app/side-bar/home/home.component.spec.ts
@@ -20,4 +20,9 @@ describe('HomeComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  
+    it("testing header", ()=>{
+    const data=fixture.nativeElement;
+    expect(data.querySelector(".content").textContent).toContain("This is the home page. Users will be able to see things posted from people they follow.")
+  })
 });
